9803facaa01726df1602380e4dcd11e3dca67ecf
denysburda <Denys.Burda@externals.adidas.com>
2023-08-29 15:24:27 +0300

    init commit

---
+++ notification-service/src/test/java/com/nfa/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

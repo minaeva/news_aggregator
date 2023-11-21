9b39375c3f51dd703bf04adf2da62351fdfc6421
minaeva <s.minaeva@gmail.com>
2023-11-21 20:03:25 +0000

    user_db with init script in docker works

---
+++ user-service/sql/init-db.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS public.reader
+(
+    id integer NOT NULL,
+    name character varying(40) NOT NULL,
+    email character varying(40) NOT NULL,
+    password character varying(400),
+    role character varying(40),
+    registration_source character varying(40),
+    PRIMARY KEY (email)
+    );
+
+CREATE TABLE IF NOT EXISTS public.keyword
+(
+    id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.reader_keyword
+(
+    reader_email character varying(40) NOT NULL,
+    keyword_id integer NOT NULL,
+    FOREIGN KEY (reader_email)
+        REFERENCES public.reader (email) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    FOREIGN KEY (keyword_id)
+        REFERENCES public.keyword (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);

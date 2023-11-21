b0c6faab5362730da4ccf7126aa8de12e2d97b13
minaeva <s.minaeva@gmail.com>
2023-11-21 16:35:33 +0000

    static final String KEYWORDS_TOPIC is now private

--- notification-service/src/main/java/com/nfa/services/ConsumerService.java
+++ notification-service/src/main/java/com/nfa/services/ConsumerService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConsumerService {
 
-    public static final String KEYWORDS_TOPIC = "keywordstopic";
+    private static final String KEYWORDS_TOPIC = "keywordstopic";
 
     @KafkaListener(topics = KEYWORDS_TOPIC, groupId = "news_consumer_group_id")
     public void consume(String message) {

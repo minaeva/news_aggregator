8069cf48486b3ae4cbbd14674c1fdff3951b4a1f
minaeva <s.minaeva@gmail.com>
2023-12-14 00:25:30 +0000

    resilience4j circuitbreaker added

--- aggregator-service/src/main/java/com/nfa/controller/ArticleController.java
+++ aggregator-service/src/main/java/com/nfa/controller/ArticleController.java
@@ -2,7 +2,10 @@ package com.nfa.controller;
 
 import com.nfa.dto.ArticleDto;
 import com.nfa.service.ArticleService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +21,18 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("/{pageNumber}/{pageSize}")
+    @CircuitBreaker(name = "myCircuitBreaker", fallbackMethod = "fallback")
     public List<ArticleDto> getArticlesPaginated(@PathVariable int pageNumber, @PathVariable int pageSize) {
         return articleService.findAll(pageNumber, pageSize);
     }
 
     @GetMapping()
+    @CircuitBreaker(name = "myCircuitBreaker", fallbackMethod = "fallback")
     public Set<ArticleDto> getReadersArticles(@RequestHeader(AUTHORIZATION) String jwtWithBearer) {
         return articleService.findAllByJwt(jwtWithBearer);
+    }
+
+    private ResponseEntity<String> fallback(Throwable t) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Fallback Response");
     }
 }

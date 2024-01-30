4502b65507a84693a2604126c1a477d6a669ae79
minaeva <s.minaeva@gmail.com>
2024-01-30 14:13:17 +0000

    build phase to contain zip as an artifact

---
+++ user-service/start_application.sh
@@ -0,0 +1,8 @@
+# Navigate to the application directory
+cd /home/ec2-user/services/user-service
+
+# Use the .env file in the current directory
+# Ensure the .env file is present and has the necessary environment variables
+
+# Start the Java application
+java -jar user-service-0.0.1-SNAPSHOT.jar

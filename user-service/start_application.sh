c59febe11cdb900be6e817ff21c2551aaad9e0d9
minaeva <s.minaeva@gmail.com>
2024-02-07 21:51:29 +0000

    triggering user service

--- user-service/start_application.sh
+++ user-service/start_application.sh
@@ -1,4 +1,4 @@
 cd /home/ec2-user/services/user-service
 #java -jar user-service-0.0.1-SNAPSHOT.jar
 #
-java -jar user-service-0.0.1-SNAPSHOT.jar > /var/log/user-service/logfile.log 2> /var/log/user-service/errorlog.log
+java -jar user-service-0.0.1-SNAPSHOT.jar > /tmp/logfile.log 2> /tmp/errorlog.log

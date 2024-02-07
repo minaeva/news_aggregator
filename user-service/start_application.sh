e47cd9cf93232a0426f63648b34e594c424bae67
minaeva <s.minaeva@gmail.com>
2024-02-07 21:28:05 +0000

    triggering user service

--- user-service/start_application.sh
+++ user-service/start_application.sh
@@ -1,8 +1,3 @@
-# Navigate to the application directory
 cd /home/ec2-user/services/user-service
-
-# Use the .env file in the current directory
-# Ensure the .env file is present and has the necessary environment variables
-
-# Start the Java application
-java -jar user-service-0.0.1-SNAPSHOT.jar
+#java -jar user-service-0.0.1-SNAPSHOT.jar
+java -jar user-service-0.0.1-SNAPSHOT.jar > /var/log/user-service/logfile.log 2> /var/log/user-service/errorlog.log

75fcaf75c1bb2609f42e8c83d64b6d19a622337b
minaeva <s.minaeva@gmail.com>
2024-02-08 09:20:13 +0000

    triggering user service

--- user-service/start_application.sh
+++ user-service/start_application.sh
@@ -1,5 +1,6 @@
+#cd /home/ec2-user/services/user-service
+#java -jar user-service-0.0.1-SNAPSHOT.jar >> /tmp/logfile.log 2>> /tmp/errorlog.log
+
+#!/bin/bash
 cd /home/ec2-user/services/user-service
-source .env
-#java -jar user-service-0.0.1-SNAPSHOT.jar
-#
-java -jar user-service-0.0.1-SNAPSHOT.jar > /tmp/logfile.log 2> /tmp/errorlog.log
+docker-compose up --build > /tmp/logfile.log 2> /tmp/errorlog.log

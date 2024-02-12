4259ccf7a69d45eafa8f7351de34ba56afa7ddc8
minaeva <s.minaeva@gmail.com>
2024-02-12 23:51:00 +0000

    add cleanup.sh to buildspec

---
+++ aggregator-service/cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -f /home/ec2-user/services/aggregator-service/aggregator-service-0.0.1-SNAPSHOT.jar
---
+++ notification-service/cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -f /home/ec2-user/services/notification-service/notification-service-0.0.1-SNAPSHOT.jar

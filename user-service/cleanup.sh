81a585a0fb0a28390f4958c8e7d5d04131c58584
minaeva <s.minaeva@gmail.com>
2024-02-14 12:40:30 +0000

    adding cleanup

---
+++ user-service/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f /home/ec2-user/services/user-service/user-service-0.0.1-SNAPSHOT.jar
+rm -f /home/ec2-user/services/user-service/docker-compose.yml
+rm -f /home/ec2-user/services/user-service/start_application.sh
+rm -f /home/ec2-user/services/user-service/cleanup.sh

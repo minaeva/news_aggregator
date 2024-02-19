b762e663028d381d6bb5fdef0c7f87247c931c7a
minaeva <s.minaeva@gmail.com>
2024-02-19 11:10:55 +0000

    update instance ip

--- frontend/js/environment.js
+++ frontend/js/environment.js
@@ -1,4 +1,4 @@
 // const IP = "http://localhost";
-const IP = "http://18.196.103.139";
+const IP = "http://52.57.165.188";
 const AGGREGATOR_SERVICE = IP + ":8071";
 const USER_SERVICE = IP + ":8073";

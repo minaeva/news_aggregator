5a0be5dbf175d83f2be1ca1bf97193c8c11f07b3
minaeva <s.minaeva@gmail.com>
2024-02-20 18:37:20 +0000

    to build the aggregator service

--- aggregator-service/deployment/scripts/start_application.sh
+++ aggregator-service/deployment/scripts/start_application.sh
@@ -2,6 +2,6 @@
 echo "Starting aggregator-service..."
 cd /home/ec2-user/services/aggregator-service
 pwd
-ls -la
+#ls -la
 docker-compose up -d --force-recreate aggregator-service
 echo "Aggregator-service started."
